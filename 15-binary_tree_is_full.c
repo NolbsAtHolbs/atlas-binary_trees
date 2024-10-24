0dfb62e7f54735349e4c0b33435241d6e7b1ac0e
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-10-23 22:50:29 -0500

    format

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -16,8 +16,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	{
 		int left_full = binary_tree_is_full(tree->left);
 		int right_full = binary_tree_is_full(tree->right);
+
 		return (left_full && right_full);
 	}
-
 	return (0); /* one child is NULL, other isn't, so tree isn't full */
 }
