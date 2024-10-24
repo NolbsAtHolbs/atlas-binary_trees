41778b6324c71114acd72537aaf765451b079b2d
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-10-23 22:30:08 -0500

    return type correction

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -21,12 +21,12 @@ int binary_tree_balance(const binary_tree_t *tree)
  * @tree: pointer to the root node of the tree to measure height
  * Return: height of the tree. If tree is NULL, return 0
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 	left_height = binary_tree_height(tree->left);
